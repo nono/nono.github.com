bb8b28c90f6f7677644f54b0f505d03feb9e3097
Bruno Michel <bmichel@menfin.info>
2014-10-12 17:05:38 +0200

    Use the same repository for source and generated files

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,12 +10,12 @@ cp public/index.xml public/atom.xml
 git add -A
 
 # Commit changes
-msg="Rebuilding site $(date)"
+msg="Rebuilding site $(LANG=C date)"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
 
 # Push source and build repos
-git push origin master
+git push origin source
 git subtree push --prefix=public git@github.com:nono/nono.github.com.git master
